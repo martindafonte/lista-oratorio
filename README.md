06534c174f78a2fa33bc2fc40aa0070277d64bea
Martín Da Fonte <martindafonte@gmail.com>
2018-04-24 15:27:19 -0300

    feat: completar modelo de usuario y readme

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
 # lista-oratorio
 Herramienta para el pasaje de lista de oratorios utilizando Trello
 
+# Estructura del proyecto
+
+## public
+Contiene los archivos que se envían al usuario para desplegarse en el navegador. Incluye HTML estático, CSS y JS
+
+## src
+Carpeta con el código fuente del servidor. 
+
+app.js es el punto de inicio de la misma. 
+board-manager.js contiene la lógica de manejo de un tablero
+
+- **helpers** Utilitarios 
+- **models** Modelo de datos y acceso a la base de datos
+- **routes** Rutas servida por Express
+- **tests** Archivos de pruebas a ejecuta por mocha
+- **views** Vistas dinámicas servidas por el servidor de Express
+- **.env** Archivo que contiene variables de ambiente y secretos de la aplicación (no commitear)
+
 ## Welcome to the Glitch BETA
 
 Click `Show` in the header to see your app live. Updates to your code will instantly deploy and update live.
--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,41 @@
+const db = require('./database');
 module.exports = class User {
+  /**
+   * Constructor of class User
+   * @param {string} username 
+   * @param {string} api_key 
+   * @param {string} token 
+   */
   constructor(username, api_key, token) {
+    console.log(username + api_key + token);
     this.username = username;
     this.api_key = api_key;
     this.token = token;
+    this._id = null;
+  }
+
+  saveUser(callback) {
+    if (this._id == null) {
+      db.users.insert(this._getData(), (err, doc) => callback(err, doc));
+    } else {
+      db.users.update({ _id: this._id }, { $set: this._getData() }, { returnUpdatedDocs: true },
+        (err, numAffected, doc) => callback(err, doc));
+    }
+  }
+
+  _getData() {
+    return { username: this.username, api_key: this.api_key, token: this.token };
+  }
+
+  static findUser(id, callback) {
+    db.users.findOne({ _id: id }, (err, data) => {
+      if (err) {
+        callback(err);
+      } else {
+        let user = new User(data.username, data.api_key, data.token);
+        user._id = data._id;
+        callback(err, user);
+      }
+    });
   }
 }
