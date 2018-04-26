03d57c429913ad716713815118aac6c1c9fd79d6
Martín Da Fonte <martindafonte@gmail.com>
2018-04-26 15:49:43 -0300

    feat: se implementa creación de lista en API

--- src/routes/list-manager-api.js
+++ src/routes/list-manager-api.js
@@ -1,19 +1,38 @@
 const express = require('express');
+const User = require('./../models/user');
+const BoardManager = require('./../board-manager');
 const router = express.Router();
 
 
-router.route("/")
-    //Crear un nuevo checklist
-    .post((request, response) => {
-        
-        response.send('No implementado aún');
-    })
-
-router.route("/close").post(
-    (request, response) => {
-        console.log(JSON.stringify(request.body));
-        response.send('No implementado aún');
+//Crear un nuevo checklist
+router.post('/', (request, response) => {
+    let token = process.env.TRELLO_TOKEN;
+    let user = new User('me', process.env.TRELLO_APIKEY, token);
+    if (!request.body.board_id) {
+        response.send('No se encontró un parámetro board_id en el pedido');
+        return;
     }
+    let manager = new BoardManager(user, request.body.board_id);
+    manager.addDateToLists(request.body.date).then(x => {
+        if (x && x.ok) response.send('Se completó la creación de la lista');
+        else response.send('Resultado de la operación: ' + JSON.stringify(x));
+    }
+    ).catch(err =>
+        response.sendStatus(500).send('Ocurrió un error al crear la lista. ' + err));
+})
+
+//
+router.post('/close', (request, response) => {
+    let token = process.env.TRELLO_TOKEN;
+    let user = new User('me', process.env.TRELLO_APIKEY, token);
+    if (!request.body.board_id) {
+        response.send('No se encontró un parámetro board_id en el pedido');
+        return;
+    }
+    let manager = new BoardManager(user, request.body.board_id);
+    console.log(JSON.stringify(request.body));
+    response.send('No implementado aún');
+}
 )
 
 module.exports = router;
