c2e8579bcdfc57fccda708847afb5b1f3fc2f92a
Martín Da Fonte <martindafonte@gmail.com>
2018-05-01 19:12:04 -0300

    test: add webhook parser as test

--- tests/logic/web-hook-parser.test.js
+++ tests/logic/web-hook-parser.test.js
@@ -2,12 +2,18 @@ const fs = require('fs');
 const assert = require('assert');
 const web_hook_parser = require('./../../src/helpers/web-hook-parser');
 
-fs.readFile('tests/data/samplewebhooks.json', 'UTF8', function (err, data) {
-  let json = JSON.parse(data);
-  json.forEach(element => {
-    let accion = element.accion;
-    let body = element.body;
-    let resultado = web_hook_parser.getWebHookAction(body.action);
-    assert.equal(resultado, web_hook_parser.ACTIONS[accion], 'La acción no coincide con el valor esperado');
-  });
-});
+describe('Web Hook Parser', function () {
+  it('Clasificar web hooks', function (done) {
+    fs.readFile('tests/data/samplewebhooks.json', 'UTF8', function procesarArchivo(err, data) {
+      let json = JSON.parse(data);
+      json.forEach(element => {
+        let accion = element.accion;
+        let body = element.body;
+        let resultado = web_hook_parser.getWebHookAction(body.action);
+        assert.equal(resultado, web_hook_parser.ACTIONS[accion], 'La acción no coincide con el valor esperado');
+      });
+      done();
+    });
+
+  })
+})
