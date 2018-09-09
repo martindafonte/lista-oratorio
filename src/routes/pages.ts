19aa4dd26cb9d5f9bdfbc56289225bf12229a74f
Martín Da Fonte <martindafonte@gmail.com>
2018-09-09 19:48:01 -0300

    feat: Se agregan páginas de About, Boards y Listas

--- src/routes/pages.ts
+++ src/routes/pages.ts
@@ -1,17 +1,28 @@
-// import index_page = require('./../../views/pages/index/index');
+//Import Mako views
 var index_view = require('./../../views/pages/index/index.marko');
+var about_view = require('./../../views/pages/about/index.marko');
+var board_view = require('./../../views/pages/boards/index.marko');
 import { User } from './../models/user';
 import { BoardManager } from './../helpers/board-manager';
 
-export function registerPages(app){
+export function registerPages(app) {
+  let token = process.env.TRELLO_TOKEN;
+  let user = new User('me', process.env.TRELLO_APIKEY, token);
+  let manager = new BoardManager(user, process.env.TRELLO_TEST_BOARD_ID);
+
+
   app.get('/', (req, res) => {
-    let token = process.env.TRELLO_TOKEN;
-    let user = new User('me', process.env.TRELLO_APIKEY, token);
-    let manager = new BoardManager(user, process.env.TRELLO_TEST_BOARD_ID);
     manager.getListsData().then(x => {
       if (x && x.logIfError()) return res.status(500);
-      let data = { board_id: process.env.TRELLO_TEST_BOARD_ID, listas: x.data };
-      res.marko(index_view, data);
+      else res.marko(index_view, { board_id: process.env.TRELLO_TEST_BOARD_ID, listas: x.data });
     });
+  });
+
+  app.get('/about', (req, res) => {
+    res.marko(about_view, {});
+  });
+
+  app.get('/boards', (req, res) => {
+    res.marko(board_view, { boards: ['lMr9XHiA', 'lMr9XHiA', 'lMr9XHiA'] });
   });
 }
