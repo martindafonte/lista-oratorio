ee8d785322bbc6014230f2b9c86ab9f5a199ab95
Martin Da Fonte <mdafonte@.sicfe.com.uy>
2018-09-09 17:51:50 -0300

    feat: Se agrega Lasso para funcionamiento de Marko en el cliente

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,21 @@ import { AddressInfo } from 'net';
 import { registerPages } from './routes/pages';
 const app = express();
 
+var isProduction = process.env.NODE_ENV === 'production';
 
+// Configure lasso to control how JS/CSS/etc. is delivered to the browser
+require('lasso').configure({
+    plugins: [
+        'lasso-marko' // Allow Marko templates to be compiled and transported to the browser
+    ],
+    outputDir: __dirname + '/../public', // Place all generated JS/CSS/etc. files into the "static" dir
+    bundlingEnabled: isProduction, // Only enable bundling in production
+    minify: isProduction, // Only minify JS and CSS code in production
+    fingerprintsEnabled: isProduction, // Only add fingerprints to URLs in production
+});
+
+
+app.use(require('lasso/middleware').serveStatic());
 
 // compress our client side content before sending it over the wire
 app.use(compression());
